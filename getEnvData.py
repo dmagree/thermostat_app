2c664e0fa124e405f520e1a4478e83804ead1e30
Daniel Magree <dmagree@gmail.com>
2018-11-03 12:09:07 -0400

    exception handling

--- getEnvData.py
+++ getEnvData.py
@@ -8,21 +8,29 @@ dbname = 'thermostat.db'
 thermostat_address = os.environ["THERMOSTAT_ADDRESS"]
 
 def getEnvData():
-	data = requests.get( 'http://'+thermostat_address+'/status')
-	#print data.text
+	data = requests.get( 'http://'+thermostat_address+'/status', timeout=1.0)
+	print data.text
 	datalist = [float(i) for i in data.text.split('|')]
 	return datalist
 
 def storeData(temp, hum, desired_temp, is_heating):
 	conn = sqlite3.connect(dbname)
 	curs = conn.cursor()
-	#print "saving:", temp, hum, desired_temp, is_heating
-	curs.execute("INSERT INTO DHT_data values(datetime('now'), (?), (?), (?), (?))", (temp, hum, desired_temp, is_heating))
+	print "saving:", temp, hum, desired_temp, is_heating
+	curs.execute("INSERT INTO DHT_data values(datetime('now'), (?), (?), (?), (?))", 
+		(temp, hum, desired_temp, is_heating))
 	conn.commit()
 	conn.close()
 
 def main():
-	datalist = getEnvData()
+	try:
+		datalist = getEnvData()
+	except requests.exceptions.Timeout:
+		print "request timed out"
+		return
+	except requests.exceptions.RequestException:
+		print "Other exception"
+		return
 	storeData(datalist[0], datalist[2], datalist[4], datalist[3])
 
 if __name__ == '__main__':
